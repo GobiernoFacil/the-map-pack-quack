44e93fecbfd68751a7bf8dc2c9dd7cf579f15d65
el coruco <el.coruco@gmail.com>
2016-05-20 23:19:52 -0500

    install laravel

---
+++ resources/views/generator.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>El generador de mapas</title>
+</head>
+<body>
+
+<script src="js/bower_components/d3/d3.js"></script>
+<script></script>
+</body>
+</html>
