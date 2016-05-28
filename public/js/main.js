b6cad28c63a386a180cdb461c967abddf66da390
el coruco <el.coruco@gmail.com>
2016-05-28 13:11:11 -0500

    add the base js for the maps

---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+// BIG MAP APP
+//
+//
+//
+
+/*
+ * [ V A R   D E F I N I T I O N ]
+ * --------------------------------------------------------------------------------
+ *
+ *
+ */
+
+//
+// [ define las variables para genera el mapa ]
+//
+//
+var MAP = {
+  div     : "map",
+  lat     : 22.442167,
+  lng     : -100.110350,
+  zoom    : 5,
+  token   : "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw",
+  maxZoom : 18,
+  id      : 'mapbox.light',
+  baseURL : 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=',
+  attribution : 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>'
+};
+
+var st = {
+    weight      : 1,
+    opacity     : 0.6,
+    fillOpacity : 0,         
+    color       : 'black',
+  };
+
+var map = L.map(MAP.div).setView([MAP.lat, MAP.lng], MAP.zoom);
+/*
+var states = L.geoJson(data, {
+      style: st
+    }).addTo(this.map);
+*/
+
+d3.json("js/json/estados.json", function(e, d){
+  states = L.geoJson(d, {
+      style: st,
+      onEachFeature : function(feature, layer) {
+        layer.on({
+        mouseover : function(){
+          //console.log(feature);
+          console.log( this.feature.properties);
+          //console.log(layer);
+        }
+        //mouseout  : this.resetHighlight.bind(this),
+        //click     : this.zoomToFeature.bind(this)
+      });
+    },
+  }).addTo(map);
+});
--- resources/views/generator.blade.php
+++ resources/views/generator.blade.php
@@ -3,10 +3,16 @@
 <head>
   <meta charset="utf-8">
   <title>El generador de mapas</title>
+  <link rel="stylesheet" type="text/css" href="js/bower_components/leaflet/dist/leaflet.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+  <!-- el mapa! -->
+  <div id="map"></div>
 
 <script src="js/bower_components/d3/d3.js"></script>
+<script src="js/bower_components/leaflet/dist/leaflet.js"></script>
+<script src="js/main.js"></script>
 <script></script>
 </body>
 </html>
